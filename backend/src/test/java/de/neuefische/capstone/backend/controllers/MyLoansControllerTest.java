5009ef8e836da2f97a2ae06d635e578a2f35fde2
saman1357 <saman1357@yahoo.com>
2023-08-02 00:28:19 +0200

    add another integration test for thrown exception in service to increase test coverage

--- backend/src/test/java/de/neuefische/capstone/backend/controllers/MyLoansControllerTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/controllers/MyLoansControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,6 +25,7 @@ class MyLoansControllerTest {
     MyLoansRepository myLoansRepository;
 
     @Test
+    @DirtiesContext
     void expectUserData_whenGetUserDataIsCalled() throws Exception {
         UserData testUserData = new UserData("0001",
                 (List.of(
@@ -45,6 +47,32 @@ class MyLoansControllerTest {
     }
 
     @Test
+    @DirtiesContext
+    void expectUserIdNotFoundException_whenGetUserDataWithWrongId() throws Exception {
+        String expectedMessage = """
+                {
+                 "message":"User Data not found for id: 0001"
+                }
+                """;
+        UserData testUserData = new UserData("0002",
+                (List.of(
+                        new Item("1001", "€ (money)"),
+                        new Item("1002", "Book"))),
+                List.of(
+                        new Person("2001", "Hanna"),
+                        new Person("2002", "Mona")),
+                List.of(
+                        new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", ""),
+                        new Loan("3002", "2002", "0001", "1001", "Fahrschule", 500, "06.06.2023", "12.12.2023"))
+        );
+        myLoansRepository.save(testUserData);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
+    }
+
+    @Test
+    @DirtiesContext
     void expectNewLoan_whenAddNewLoan() throws Exception {
         UserData userDataToUpdate = new UserData("0001",
                 new ArrayList<>(List.of(
@@ -70,7 +98,39 @@ class MyLoansControllerTest {
     }
 
     @Test
-    void expectLoan_whenGetLoanDetailsIsCalles() throws Exception {
+    @DirtiesContext
+    void expectUserIdNotFoundException_whenAddNewLoanWithWrongUserId() throws Exception {
+        String expectedMessage = """
+                {
+                 "message":"User Data not found for id: 0001"
+                }
+                """;
+        UserData userDataToUpdate = new UserData("0002",
+                new ArrayList<>(List.of(
+                        new Item("1001", "€ (money)"),
+                        new Item("1002", "Book"))),
+                new ArrayList<>(List.of(
+                        new Person("2001", "Hanna"),
+                        new Person("2002", "Mona"))),
+                new ArrayList<>(List.of(
+                        new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", "")))
+        );
+        LoanWithoutId newLoanWithoutId = new LoanWithoutId("2002", "0001", "1001", "Fahrschule", 500, "06.06.2023", "12.12.2023");
+
+        myLoansRepository.save(userDataToUpdate);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String newLoanWithoutIdJson = objectMapper.writeValueAsString(newLoanWithoutId);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/myloans")
+                        .contentType(MediaType.APPLICATION_JSON).content(newLoanWithoutIdJson))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
+
+    }
+
+    @Test
+    @DirtiesContext
+    void expectLoan_whenGetLoanDetailsIsCalled() throws Exception {
         String loanId = "3001";
         Loan expectedLoan = new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", "");
         UserData testUserData = new UserData("0001",
@@ -93,7 +153,35 @@ class MyLoansControllerTest {
     }
 
     @Test
-    void expectNoSuchElementException_whenLoanIdNotFound() throws Exception {
+    @DirtiesContext
+    void expectUserIdNotFoundException_whenGetLoanDetailsWithWrongUserId() throws Exception {
+        String expectedMessage = """
+                {
+                 "message":"User Data not found for id: 0001"
+                }
+                """;
+        String loanId = "3001";
+        Loan expectedLoan = new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", "");
+        UserData testUserData = new UserData("0002",
+                (List.of(
+                        new Item("1001", "€ (money)"),
+                        new Item("1002", "Book"))),
+                List.of(
+                        new Person("2001", "Hanna"),
+                        new Person("2002", "Mona")),
+                List.of(
+                        expectedLoan,
+                        new Loan("3002", "2002", "0001", "1001", "Fahrschule", 500, "06.06.2023", "12.12.2023"))
+        );
+        myLoansRepository.save(testUserData);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans/" + loanId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
+    }
+
+    @Test
+    @DirtiesContext
+    void expectLoanIdNotFoundException_whenGetLoanDetailsWithWrongId() throws Exception {
         String expectedMessage = """
                 {
                  "message":"Loan not found for id: 123"
@@ -112,30 +200,6 @@ class MyLoansControllerTest {
         );
         myLoansRepository.save(testUserData);
         mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans/123"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
-    }
-
-    @Test
-    void expectNoSuchElementException_whenUserIdNotFound() throws Exception {
-        String expectedMessage = """
-                {
-                 "message":"User Data not found for id: 0001"
-                }
-                """;
-        UserData testUserData = new UserData("0002",
-                (List.of(
-                        new Item("1001", "€ (money)"),
-                        new Item("1002", "Book"))),
-                List.of(
-                        new Person("2001", "Hanna"),
-                        new Person("2002", "Mona")),
-                List.of(
-                        new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", ""),
-                        new Loan("3002", "2002", "0001", "1001", "Fahrschule", 500, "06.06.2023", "12.12.2023"))
-        );
-        myLoansRepository.save(testUserData);
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
     }
