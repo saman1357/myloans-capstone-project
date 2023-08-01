a8a9543be75d16936a6dbeb1a8f77bdf1898b866
saman1357 <saman1357@yahoo.com>
2023-08-02 00:05:47 +0200

    add another integration test for thrown exception in service to increase test coverage

--- backend/src/test/java/de/neuefische/capstone/backend/controllers/MyLoansControllerTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/controllers/MyLoansControllerTest.java
@@ -71,7 +71,7 @@ class MyLoansControllerTest {
 
     @Test
     void expectLoan_whenGetLoanDetailsIsCalles() throws Exception {
-        String loanId="3001";
+        String loanId = "3001";
         Loan expectedLoan = new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", "");
         UserData testUserData = new UserData("0001",
                 (List.of(
@@ -87,7 +87,7 @@ class MyLoansControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         String expectedJson = objectMapper.writeValueAsString(expectedLoan);
         myLoansRepository.save(testUserData);
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans/"+loanId))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans/" + loanId))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(expectedJson));
     }
@@ -112,6 +112,30 @@ class MyLoansControllerTest {
         );
         myLoansRepository.save(testUserData);
         mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans/123"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
+    }
+
+    @Test
+    void expectNoSuchElementException_whenUserIdNotFound() throws Exception {
+        String expectedMessage = """
+                {
+                 "message":"User Data not found for id: 0001"
+                }
+                """;
+        UserData testUserData = new UserData("0002",
+                (List.of(
+                        new Item("1001", "€ (money)"),
+                        new Item("1002", "Book"))),
+                List.of(
+                        new Person("2001", "Hanna"),
+                        new Person("2002", "Mona")),
+                List.of(
+                        new Loan("3001", "0001", "2001", "1002", "Der kleine Prinz", 1, "01.01.2023", ""),
+                        new Loan("3002", "2002", "0001", "1001", "Fahrschule", 500, "06.06.2023", "12.12.2023"))
+        );
+        myLoansRepository.save(testUserData);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/myloans"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andExpect(MockMvcResultMatchers.content().json(expectedMessage));
     }
