3e550abfd892d227ced8f79c2450a9d9169f6bc0
saman1357 <saman1357@yahoo.com>
2023-08-14 19:52:10 +0200

    remove unused import statement

--- backend/src/test/java/de/neuefische/capstone/backend/security/MyLoansUserServiceTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/security/MyLoansUserServiceTest.java
@@ -2,8 +2,6 @@ package de.neuefische.capstone.backend.security;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
