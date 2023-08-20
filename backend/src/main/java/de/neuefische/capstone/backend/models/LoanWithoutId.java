7ebabfacc4ae5d7e2dbca5a4b87fb7831d9a4b21
saman1357 <saman1357@yahoo.com>
2023-08-21 00:15:13 +0200

    fix code smell

--- backend/src/main/java/de/neuefische/capstone/backend/models/LoanWithoutId.java
+++ backend/src/main/java/de/neuefische/capstone/backend/models/LoanWithoutId.java
@@ -1,7 +1,6 @@
 package de.neuefische.capstone.backend.models;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -17,7 +16,6 @@ public class LoanWithoutId {
     @NotBlank(message ="otherPartyId can't be blank!")
     private String itemId;
     private String description;
-    @NotNull
     @Positive(message = "amount should be greater than 0")
     private int amount;
     @Pattern(regexp = "^((19|2\\d)\\d{2})-(0[1-9]|1[012])-(0[1-9]|[12]\\d|3[01])$", message = "no valid date!")
