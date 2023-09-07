38fc5d33cbaa8e9ad5e7493337a41e4cabfe68f0
saman1357 <saman1357@yahoo.com>
2023-09-07 22:57:59 +0200

    bugfix in continuous deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -148,6 +148,7 @@ export default function App() {
                 setUser(prevState => ({...prevState, id: response.data.id, username: response.data.username}));
             });
     }
+
     function handleSignUp(username: string, password: string) {
         const userWithoutId: UserWithoutId = {username, password};
         axios.post("/api/user/sign-up", userWithoutId)
